1fad977d68e50558ea722fc7dac35ac1cd26fab6
rashmika <rashmikadil2023@gmail.com>
2025-01-29 20:28:25 +0530

    This code will : item card navigation 2

--- src/components/about.js
+++ src/components/about.js
@@ -3,13 +3,12 @@ const About = () => {
       <div className="bg-gray-100 py-12 px-6 md:px-20 flex flex-col items-center">
         <h2 className="text-3xl font-bold text-gray-900 mb-6">About Us</h2>
         <p className="text-gray-700 text-lg leading-relaxed text-center max-w-4xl">
-        "Discover the beauty of Sri Lanka like never before with our comprehensive tourism destination rating website. Designed to guide travelers and locals alike, our platform provides detailed insights into the best attractions across the island. From pristine beaches to lush tea plantations, ancient temples to vibrant cities, explore a curated list of destinations rated by visitors.
-        </p>
+        "Discover the beauty of Sri Lanka like never before with our  website. 
+        Through our website you can find the best activities to do in Sri Lanka. 
+        We invite you to connect with our website and share your travel experience"  </p>
         <p className="text-gray-700 text-lg leading-relaxed text-center mt-4 max-w-4xl">
-        Key features include real-time weather updates, user reviews, ratings, and tips to enhance your travel experience. Whether you're planning your next adventure or just exploring the wonders of Sri Lanka, our website is your ultimate travel companion. Navigate easily, discover hidden gems, and make informed decisions with authentic insights tailored just for you.
 
-Embark on a journey to explore the Pearl of the Indian Ocean, one rating at a time!"
-        </p>
+     </p>
         <button
           className="bg-gray-900 text-white px-6 py-2 mt-8 rounded-full hover:bg-gray-800 transition-all"
           
